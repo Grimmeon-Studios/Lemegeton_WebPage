b476e0e62e879c06681805f958ddc8622a27ba75
JU4NP4260 <82065985+JU4NP4260@users.noreply.github.com>
2023-10-09 12:57:17 -0500

    Images fix 2

--- NamelssSite/src/js/DemonsList_script.js
+++ NamelssSite/src/js/DemonsList_script.js
@@ -10,27 +10,27 @@ function openModal(element) {
         document.getElementById("modalTitle").textContent = "Belfegor";
         document.getElementById("modalContent").textContent = "Contenido único para el elemento 1.";
       
-        document.getElementById("modalImage").src = "imagen1.jpg";
+        document.getElementById("modalImage").src = "https://github.com/Grimmeon-Studios/Lemegeton_WebPage/blob/main/NamelssSite/src/imgs/Belphegor%202.jpg?raw=true";
     } else if (modalNumber === "2") {
         document.getElementById("modalTitle").textContent = "Behamut";
         document.getElementById("modalContent").textContent = "Contenido único para el elemento 2.";
       
-        document.getElementById("modalImage").src = "imagen1.jpg";
+        document.getElementById("modalImage").src = "https://github.com/Grimmeon-Studios/Lemegeton_WebPage/blob/main/NamelssSite/src/imgs/bahamut%202.jpg?raw=true";
     } else if (modalNumber === "3") {
-        document.getElementById("modalTitle").textContent = "Infierno de la escarcha";
+        document.getElementById("modalTitle").textContent = "Belial";
         document.getElementById("modalContent").textContent = "Contenido único para el elemento 3.";
       
-        document.getElementById("modalImage").src = "imagen1.jpg";
+        document.getElementById("modalImage").src = "https://github.com/Grimmeon-Studios/Lemegeton_WebPage/blob/main/NamelssSite/src/imgs/Belial%202.jpg?raw=true";
     } else if (modalNumber === "4") {
-        document.getElementById("modalTitle").textContent = "Infierno de la soledad";
+        document.getElementById("modalTitle").textContent = "Imp";
         document.getElementById("modalContent").textContent = "Una enorme bestia que habita entre los círculos de la ira y la violencia, está bajo el mando de Satanás y es el encargado de mantener en sufrimiento las almas de ambos círculos. Es fuerte, resistente y altamente agresivo, por lo que debe combatirse con astucia y agilidad.";
 
-        document.getElementById("modalImage").src = "imagen1.jpg";
+        document.getElementById("modalImage").src = "https://github.com/Grimmeon-Studios/Lemegeton_WebPage/blob/main/NamelssSite/src/imgs/imp%202.png?raw=true";
     } else if (modalNumber === "5") {
-        document.getElementById("modalTitle").textContent = "Infierno de la soberbia";
+        document.getElementById("modalTitle").textContent = "Íncubo";
         document.getElementById("modalContent").textContent = "Contenido único para el elemento 2.";
         // Agrega condiciones para otros números de modal si es necesario
-        document.getElementById("modalImage").src = "imagen1.jpg";
+        document.getElementById("modalImage").src = "https://github.com/Grimmeon-Studios/Lemegeton_WebPage/blob/main/NamelssSite/src/imgs/Incubo%202.jpg?raw=true";
     }
 }
 
